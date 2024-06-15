da1d1c4a3e615062af21f53644b3e9155c229d66
HimawanRakha <himawanrakha11@gmail.com>
2024-06-15 23:49:04 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -11,3 +11,13 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+//search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
